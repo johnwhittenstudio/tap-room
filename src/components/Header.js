5ff9ddb1cae0a9ef829f6ac5e4c0d67d3b70152f
John Whitten <johnwhitten.studio@gmail.com>
2022-06-02 13:50:36 -0700

    create keg.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,22 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Header = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark">
+    <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" style={{color: "lightblue"}} href="/"><h1><em>Michelob Angelo's Pietàproom</em></h1></a>
+        <a className="navbar-brand" style={{color: "orangered"}} href="/"><h1><em>Michelob Angelo's Pietàproom</em></h1></a>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" style={{color: "lightblue"}} aria-current="page" href="/">Home</a>
+              <a className="nav-link active" style={{color: "orangered"}} aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" style={{color: "lightblue"}} href="/">Menu</a>
+              <a className="nav-link" style={{color: "orangered"}} href="/">Menu</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" style={{color: "lightblue"}} href="/">Locations</a>
+              <a className="nav-link" style={{color: "orangered"}} href="/">Locations</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" style={{color: "lightblue"}} href="/">Contact</a>
+              <a className="nav-link" style={{color: "orangered"}} href="/">Contact</a>
             </li>
           </ul>
         </div>
---
+++ src/components/Keg.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+function Keg(props) {
+  const stockCheck = props.pints === 0 ? "Out of Stock" : props.pints;
+  
+  return (
+    <React.Fragment>
+      <div onClick= {() => props.whenKegClicked(props.id)}>
+        <h3>{props.brand} - {props.name}</h3>
+        <p>ABV: {props.abv}%</p>
+        <p>IBU: ${props.ibu}</p>
+        <p>Price: {props.price}</p>
+        <p>Pints available: {stockCheck}</p>
+        <hr />
+      </div>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  brand: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  abv: PropTypes.number.isRequired,
+  ibu: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  pints: PropTypes.number.isRequired,  
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
+}
+
+export default Keg;
