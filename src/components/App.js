61b764eb25d7389de2ce57c3adf83966e7a2985b
John Whitten <johnwhitten.studio@gmail.com>
2022-06-02 13:40:57 -0700

    create app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
-
-import './App.css';
+import '../App.css';
+import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="text-center" style={{backgroundColor: '#212529'}}>
+        <Header />
+      </div>
+      {/* <div className="container mb-5">
+        <div className="text-center">
+          <MerchControl />
+        </div>
+      </div> */}
+    </React.Fragment>
+    
   );
 }
 
