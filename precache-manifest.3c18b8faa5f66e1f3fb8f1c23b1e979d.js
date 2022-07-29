a9cd8313bb4956c10e64c08edcc72f53e90f7c3f
John Whitten <johnwhitten.studio@gmail.com>
2022-07-29 08:55:55 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3c18b8faa5f66e1f3fb8f1c23b1e979d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc1654f96014fc2e291fa44d5a9d938",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "393dc3a6905ed0eb99eb",
+    "url": "/tap-room/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "edd7311a16138a9f447d",
+    "url": "/tap-room/static/css/main.f83cf3d3.chunk.css"
+  },
+  {
+    "revision": "393dc3a6905ed0eb99eb",
+    "url": "/tap-room/static/js/2.cf4376fd.chunk.js"
+  },
+  {
+    "revision": "657778f7c364fe07a69a",
+    "url": "/tap-room/static/js/3.19e5904c.chunk.js"
+  },
+  {
+    "revision": "edd7311a16138a9f447d",
+    "url": "/tap-room/static/js/main.d1d399ae.chunk.js"
+  },
+  {
+    "revision": "a447ecde6ab893d364f9",
+    "url": "/tap-room/static/js/runtime-main.f0cb3929.js"
+  }
+]);
