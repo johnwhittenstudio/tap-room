81e417ddb784d33bfda567e2a455d72de5f90fb5
John Whitten <johnwhitten.studio@gmail.com>
2022-10-09 16:48:01 -0700

    adjust navbar justification

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = (props) => {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" id="logo" style={{color: "orangered"}} href="/"><h2><em>Michelob Angelo's Pietàproom</em></h2><p><em>International Artisanal Tap House</em></p></a>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse d-flex justify-content-end align-items-center me-5" id="navbarNav">
           <ul className="navbar-nav">
-            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            {/* &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; */}
 
             <li className="nav-item">
               <a className="nav-link active" style={{color: "orangered"}} aria-current="page" href="/">Home</a>
